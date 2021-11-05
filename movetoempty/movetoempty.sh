9d72791618e7c55312837c9ce6437c83e3c2ae0b
user18130814200115-2 <66143190+user18130814200115-2@users.noreply.github.com>
2021-11-05 13:13:48 +0100

    Create movetoempty.sh

---
+++ movetoempty/movetoempty.sh
@@ -0,0 +1,3 @@
+raw=$(ristate -vt & sleep 0.2 && kill $! )
+empty=$(printf "%s" "$raw" | grep -o "[0-9]" | sort -n  | awk '{for(i=p+1; i<$1; i++) print i} {p=$1}' | xargs -i_ printf "2^(_-1)\n" | bc)
+riverctl set-view-tags $empty
