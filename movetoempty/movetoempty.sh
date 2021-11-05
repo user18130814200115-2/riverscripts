b0b567142ce8ac3b1f15a2a58b4903cf94d72577
user18130814200115-2 <66143190+user18130814200115-2@users.noreply.github.com>
2021-11-05 13:18:41 +0100

    Update movetoempty.sh

--- movetoempty/movetoempty.sh
+++ movetoempty/movetoempty.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 raw=$(ristate -vt & sleep 0.2 && kill $! )
 empty=$(printf "%s" "$raw" | grep -o "[0-9]" | sort -n  | awk '{for(i=p+1; i<$1; i++) print i} {p=$1}' | xargs -i_ printf "2^(_-1)\n" | bc)
 riverctl set-view-tags $empty
